1d36dbe58124ba0c13a0873a3b285671673f24f5
TalaatHarb <mohamed.talaatharb@eng.asu.ed.eg>
2017-02-17 15:50:40 +0200

    Fixed The mathematical representation of the Sinc function

--- src/io/github/talaatharb/function/SincFunction.java
+++ src/io/github/talaatharb/function/SincFunction.java
@@ -3,13 +3,13 @@ package io.github.talaatharb.function;
 public class SincFunction extends AbstractFunction {
 	
 	public SincFunction() {
-		this.expression = "sin(a * x) / x";
+		this.expression = "sin(a * x) / (a * x)";
 	}
 
 	@Override
 	public double applyAsDouble(Double x) {
 		double a = parameters.get(PARAMETER1_NAME);
-		return Math.sin(a * x) / x;
+		return Math.sin(a * x) / (a * x);
 	}
 
 }
